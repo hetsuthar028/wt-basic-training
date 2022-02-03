0b4cc447cc20966a6a6de26f689a29655ef0750e
hetsuthar028 <hetmewada028@gmail.com>
2022-02-03 20:29:14 +0530

    React D 4 | DOM Events
    onChange, onFocus, onBlur, onKeyUp, onKeyDown, onKeyPress, onSubmit, e.preventDefault

--- reactjs-training/project1/src/components/Inputs.js
+++ reactjs-training/project1/src/components/Inputs.js
@@ -6,11 +6,40 @@ class Inputs extends Component {
         super();
         this.state = {
             inputData: '',
+            extendedFocusClass: '',
         }
     }
 
     handleChange = (e) => {
         this.setState({inputData: e.target.value});
+    }
+
+    handleFocus = (e) => {
+        // @Approach - 1 | Modifying the styles of DOM Element directly
+        e.target.style.backgroundColor = 'lightgreen';
+
+        // @Approach - 2 | Changing the className
+        // this.setState({extendedFocusClass: 'bg-primary'});
+    }
+
+    handleBlur = (e) => {
+        // @Approach - 1 | Modifying the styles of DOM Element directly
+        e.target.style.backgroundColor = 'transparent';
+
+        // @Approach - 2 | Changing the className
+        // this.setState({extendedFocusClass: ''});
+    }
+
+    handleKeyUp = (e) => {
+        console.log("onKeyUp()", e.key, e.keyCode);
+    }
+
+    handleKeyPress = (e) => {
+        console.log("onKeyPress()", e.key, e.keyCode);
+    }
+
+    handleKeyDown = (e) => {
+        console.log("onKeyDown()", e.key, e.keyCode);
     }
 
     render() {
@@ -23,9 +52,14 @@ class Inputs extends Component {
                             type="text" 
                             id='username' 
                             onChange={this.handleChange} 
+                            onFocus={this.handleFocus}
+                            onBlur={this.handleBlur}
+                            onKeyUp={this.handleKeyUp}
+                            onKeyDown={this.handleKeyDown}
+                            onKeyPress={this.handleKeyPress}
                             value={this.state.inputData} 
                             placeholder='Eg. hetsuthar028'
-                            className='form-control'
+                            className={'form-control' + ' ' + this.state.extendedFocusClass}
                         />
                         {this.state.inputData ? (<h5 className='m-4'>Welcome <span className='text-primary'>@{this.state.inputData}</span></h5>) : null}
                     </div>
